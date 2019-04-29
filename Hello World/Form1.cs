9df14055f780042a8ba1c9a72c308227acbb8f36
Sam Titterington <rc0033996@student.riversidecollege.ac.uk>
2019-04-29 15:01:23 +0100

    hello

--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -19,7 +19,12 @@ namespace Hello_World
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello " + txtBxName.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
